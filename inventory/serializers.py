af3ca63d70ecdddef435d11a45a09d0072c12eca
Eniz Gülek <enizgulek@gmail.com>
2018-08-06 11:57:33 +0300

    nested serializers

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -2,25 +2,31 @@ from rest_framework import serializers
 from . import models
 
 
-class BuildingSerializer(serializers.ModelSerializer):
-    class Meta:
-        fields = ('id', 'name',)
-        model = models.Building
-
-
-class ApartmentSerializer(serializers.ModelSerializer):
-    class Meta:
-        fields = ('id', 'number',)
-        model = models.Apartment
-
-
-class RoomSerializer(serializers.ModelSerializer):
-    class Meta:
-        fields = ('id', 'name',)
-        model = models.Room
-
-
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('id', 'name', 'price',)
         model = models.Product
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True)
+
+    class Meta:
+        fields = ('id', 'name', 'products')
+        model = models.Room
+
+
+class ApartmentSerializer(serializers.ModelSerializer):
+    rooms = RoomSerializer(many=True)
+
+    class Meta:
+        fields = ('id', 'number', 'rooms')
+        model = models.Apartment
+
+
+class BuildingSerializer(serializers.ModelSerializer):
+    apartments = ApartmentSerializer(many=True)
+
+    class Meta:
+        fields = ('id', 'name', 'apartments')
+        model = models.Building
